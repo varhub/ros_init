d6df7b9cacbb6bc39a7e7ae465df491708cfbd71
Victor Arribas <v.arribas.urjc@gmail.com>
2014-10-03 16:42:41 +0200

    README.m

---
+++ README.m
@@ -0,0 +1,28 @@
+== ros_init
+ros_init.sh is a script that serves a little X based ROS environment.
+It has been created for chroot environments. So the idea is to allow
+multiple instances.
+
+State: BETA 
+
+== requirements
+* ROS, as logic
+* X11
+  * xterm
+  * x-terminal-emulator (a.k.a your favourite terminal ;))
+* Minimal bash commands
+
+== use it
+Instead of put 'source /opt/ros/<version>/setup.sh' into your .bashrc 
+or write it in each new terminal (you can avoid it in gnome-terminal 
+with Ctrl+Shift+T), this alternative will run 'roscore' and a little 
+xterm as a terminal launcher.
+
+
+== for chroot
+I'm using schroot and it automatically mounts my home.
+And, for an unattended start, I have added a little trap 
+in my ~/.profile 'df -h 2>/dev/null || . ros_workspace/ros_init.sh'
+A chroot environment can't handle df command, so it would fail.
+Therefore, it will load ros in my chroots but won't do it in the host.
+
